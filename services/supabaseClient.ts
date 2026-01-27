ed5e2863e6abd51c5b513251869333de06902fd6
Leandro Marques <leandromarques@grupometarh.local>
2026-01-27 16:12:05 -0300

    conexão com o supabase

--- services/supabaseClient.ts
+++ services/supabaseClient.ts
@@ -1,8 +1,10 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// Defina manualmente as variáveis para teste local ou produção
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://cnwxpqttflndiuqymtmw.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNud3hwcXR0ZmxuZGl1cXltdG13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MTUzMzMsImV4cCI6MjA3OTM5MTMzM30.-p59cN_rT2e7c5evmFX6-pdOpRBHJKhlrfF7V8947hk';
 
 // Só inicializa se tivermos os parâmetros necessários
 export const isSupabaseConfigured = !!(supabaseUrl && supabaseAnonKey);
